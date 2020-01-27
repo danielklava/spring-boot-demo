c42c34ad2c68681807a648ec199ed5f16c4bc95f
Daniel Klava <klava.daniel@gmail.com>
2020-01-27 11:09:35 -0300

    Initial commit

---
+++ src/test/java/br/com/demo/partnersdigital/PartnersdigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.partnersdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnersdigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
